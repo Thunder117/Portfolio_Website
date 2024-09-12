0a63b405b8f22d740d31386e83f07e53fe15ed52
ChristopherHernandez117 <105824044+Thunder117@users.noreply.github.com>
2024-09-11 21:41:33 -0600

    added styling

--- scripts.js
+++ scripts.js
@@ -2,13 +2,29 @@ function toggleMobileMenu(){
 	document.getElementById("menu").classList.toggle("active");
 }
 
-const observer = new IntersectionObserver((entries) => {
+const hiddenObserver = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
         if(entry.isIntersecting) {
             entry.target.classList.add('show');
+        } else {
+            entry.target.classList.remove('show');
+        }
+    });
+});
+
+const barObserver = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry.target, entry.isIntersecting); // Debugging
+        if (entry.isIntersecting) {
+            entry.target.classList.add('longer');
+        } else {
+            entry.target.classList.remove('longer');
         }
     });
 });
 
 const hiddenElement = document.querySelectorAll(".hidden");
-hiddenElement.forEach((el) => observer.observe(el));
+hiddenElement.forEach((el) => hiddenObserver.observe(el));
+
+const barElement = document.querySelectorAll(".bar");
+barElement.forEach((el) => barObserver.observe(el));
